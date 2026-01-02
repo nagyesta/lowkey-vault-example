ac8c77c82ea11503d277de2347a17db7bdd36e78
Esta Nagy <nagyesta@gmail.com>
2026-01-02 23:56:30 +0100

    Update AzureAccessTestDockerConfiguration.java
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- java-sdk/src/test/java/com/github/nagyesta/lowkeyvault/example/AzureAccessTestDockerConfiguration.java
+++ java-sdk/src/test/java/com/github/nagyesta/lowkeyvault/example/AzureAccessTestDockerConfiguration.java
@@ -35,7 +35,7 @@ public class AzureAccessTestDockerConfiguration extends AzureAccessCommonTestCon
     private final LowkeyVaultContainer lowkeyVaultContainer;
 
     public AzureAccessTestDockerConfiguration() {
-        final DockerImageName imageName = DockerImageName.parse("nagyesta/lowkey-vault:7.0.2");
+        final DockerImageName imageName = DockerImageName.parse("nagyesta/lowkey-vault:7.0.3");
         lowkeyVaultContainer = lowkeyVault(imageName)
                 .hostTokenPort(HOST_TOKEN_PORT)
                 .logicalPort(HOST_PORT)
