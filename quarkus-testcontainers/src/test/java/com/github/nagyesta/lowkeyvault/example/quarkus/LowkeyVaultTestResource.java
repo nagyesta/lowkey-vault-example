291c4e037e8d7d5be0a1ce6f63d2179b26b280a0
Esta Nagy <nagyesta@gmail.com>
2026-06-08 22:38:00 +0200

    chore: Maintenance - Fix broken build
    {patch}
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- quarkus-testcontainers/src/test/java/com/github/nagyesta/lowkeyvault/example/quarkus/LowkeyVaultTestResource.java
+++ quarkus-testcontainers/src/test/java/com/github/nagyesta/lowkeyvault/example/quarkus/LowkeyVaultTestResource.java
@@ -17,7 +17,7 @@ public class LowkeyVaultTestResource implements QuarkusTestResourceLifecycleMana
     @Override
     public Map<String, String> start() {
         mySQLContainer = new MySQLContainer(DockerImageName.parse("mysql:9.7.0"));
-        lowkeyVaultContainer = lowkeyVault(DockerImageName.parse("nagyesta/lowkey-vault:4.1.0-ubi9-minimal"))
+        lowkeyVaultContainer = lowkeyVault(DockerImageName.parse("nagyesta/lowkey-vault:7.3.0-ubi10-minimal"))
                 .dependsOnContainer(mySQLContainer, sql -> Map.of(
                         "jdbc-url", mySQLContainer.getJdbcUrl(),
                         "jdbc-user", mySQLContainer.getUsername(),
